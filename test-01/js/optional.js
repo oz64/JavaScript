e9394647162ee680de7116f26a454b9ea175a64a
Oscar Delgadillo Tovar <oscar_oz_d@hotmail.com>
2015-11-30 14:31:24 -0400

    for optional points

---
+++ test-01/js/optional.js
@@ -0,0 +1,21 @@
+/**
+ * Created by oscardelgadillo on 11/30/2015.
+ */
+
+/***
+ * This function recive a paragraph and display in console the paragraph with vowels replace
+ * with '*' character
+ * @param paragraph
+ */
+replaceInParagraph = function(paragraph){
+
+    var character = '*';
+
+    var para = paragraph.replace(/a/g,character);
+    para = para.replace(/e/g,character);
+    para = para.replace(/i/g,character);
+    para = para.replace(/o/g,character);
+    para = para.replace(/u/g,character);
+
+     console.log(para);
+};
